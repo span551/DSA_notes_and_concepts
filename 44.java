5534b7f4acfd47aa0656d552677998e24064cb71
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:13:18 +0530

    Create 44.java

---
+++ 44.java
@@ -0,0 +1,12 @@
+int secondLargest(int[] arr) {
+    int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+    for (int num : arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num != first) {
+            second = num;
+        }
+    }
+    return second;
+}
