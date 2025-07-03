baf06f55cdbe8988712c449c674f94e12decde1d
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 19:37:16 +0530

    Create bfyry.java

---
+++ bfyry.java
@@ -0,0 +1,17 @@
+void reorderList(ListNode head) {
+    if (head == null) return;
+    ListNode slow = head, fast = head.next;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    ListNode second = slow.next;
+    slow.next = null;
+    second = reverse(second);
+    ListNode first = head;
+    while (second != null) {
+        ListNode t1 = first.next, t2 = second.next;
+        first.next = second; second.next = t1;
+        first = t1; second = t2;
+    }
+}
