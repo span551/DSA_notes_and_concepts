8b20d12a7a9da6956f0b98ed2bf1542a9397075b
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:29:20 +0530

    Create 4597.java

---
+++ 4597.java
@@ -0,0 +1,9 @@
+int countInversions(int[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) count++;
+        }
+    }
+    return count;
+}
