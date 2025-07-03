f8a0519998a6f27c0887dfaecb8d81487edded07
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:47:42 +0530

    Create hjcui.java

---
+++ hjcui.java
@@ -0,0 +1,8 @@
+int[][] transpose(int[][] matrix) {
+    int rows = matrix.length, cols = matrix[0].length;
+    int[][] transposed = new int[cols][rows];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            transposed[j][i] = matrix[i][j];
+    return transposed;
+}
