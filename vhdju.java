553862a2ccdb6f118ce65ff6e723acc5c31daed6
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:51:57 +0530

    Create vhdju.java

---
+++ vhdju.java
@@ -0,0 +1,13 @@
+boolean canPartition(int[] nums) {
+    int sum = Arrays.stream(nums).sum();
+    if (sum % 2 != 0) return false;
+    int target = sum / 2;
+    boolean[] dp = new boolean[target + 1];
+    dp[0] = true;
+    for (int num : nums) {
+        for (int i = target; i >= num; i--) {
+            dp[i] = dp[i] || dp[i - num];
+        }
+    }
+    return dp[target];
+}
