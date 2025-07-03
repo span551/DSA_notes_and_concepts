19419400c5fc276734733d75c3d2ed3e3eb2fd0a
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:14:47 +0530

    Create g124.java

---
+++ g124.java
@@ -0,0 +1,8 @@
+int maxSubArraySum(int[] arr) {
+    int maxSoFar = arr[0], maxEndingHere = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+}
