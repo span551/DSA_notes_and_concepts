d8bc0216757cd273befb96a5937c631a1f44cfed
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:28:21 +0530

    Create 45v.java

---
+++ 45v.java
@@ -0,0 +1,9 @@
+int findPeak(int[] arr) {
+    int left = 0, right = arr.length - 1;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] > arr[mid + 1]) right = mid;
+        else left = mid + 1;
+    }
+    return arr[left];
+}
