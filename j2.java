8505e1c3ef873321eee0800fe5cca52b61e0a319
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-03 18:13:56 +0530

    Create j2.java

---
+++ j2.java
@@ -0,0 +1,9 @@
+void moveZerosToEnd(int[] arr) {
+    int index = 0;
+    for (int num : arr) {
+        if (num != 0) arr[index++] = num;
+    }
+    while (index < arr.length) {
+        arr[index++] = 0;
+    }
+}
