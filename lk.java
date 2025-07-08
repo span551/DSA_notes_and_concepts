f19c1688810da4a19dacf2c2f77a562caffccdb0
Spandan Keche <146474627+span551@users.noreply.github.com>
2025-07-08 21:44:28 +0530

    Create lk.java

---
+++ lk.java
@@ -0,0 +1,8 @@
+public int maxSubArray(int[] nums) {
+    int max = nums[0], current = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        current = Math.max(nums[i], current + nums[i]);
+        max = Math.max(max, current);
+    }
+    return max;
+}
